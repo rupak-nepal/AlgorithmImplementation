1c29ad17decfeb234ff68e855ae121f792f21d68
rupak <nepal.rupak@gmail.com>
2018-01-05 23:43:46 +0545

    plot polygon dynamically.

--- point-inside-polygon/src/main/java/np/com/rupak/point/inside/polygon/ui/ViewPolygonWithTestPoint.java
+++ point-inside-polygon/src/main/java/np/com/rupak/point/inside/polygon/ui/ViewPolygonWithTestPoint.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import javax.swing.JFrame;
 import np.com.rupak.point.inside.polygon.point.Point;
 
@@ -12,8 +13,57 @@ import np.com.rupak.point.inside.polygon.point.Point;
  * @author rupak
  */
 public class ViewPolygonWithTestPoint extends JFrame {
+    
+    public static Point lowestPoint;
+    public static ArrayList<Point> listOfPoint;
+    public static Map<Point[], double[]> initialMap;
+    public static Map<Point[], double[]> finalalMap;
+    public static Point [] pointsOtherThanLowestPoint;
+    public static double [] angleWithOtherPointsExceptLowestPoint;
 
-    public static ArrayList getPolygonPoints() {
+    public ArrayList getLowestPoint(ArrayList<Point> list) {
+
+        Point pLowest = list.get(0);
+        for (Point pp : list) {
+            if (pLowest.getY() >= pp.getY()) {
+                pLowest = pp;
+            }
+        }
+        lowestPoint.setX(pLowest.getX());
+        lowestPoint.setY(pLowest.getY());
+        System.out.println("Smallest point is : (" + pLowest.getX() + "," + pLowest.getY() +")");
+        int i = 0;
+        for(Point pointt : list){
+            if(pointt.getX() == lowestPoint.getX()
+                    && pointt.getY() == lowestPoint.getY()){
+                System.out.println("Equal case only once. ");
+            }
+            else{
+                pointsOtherThanLowestPoint[i] = pointt;
+                angleWithOtherPointsExceptLowestPoint[i] = 
+                    getAngleBetweenLowestPointAndTargetPoint(pointt);
+                ++i;
+            }
+        }
+        
+        //getArrayOfAnles with otherpoints...
+        for(int j = 0 ; j<pointsOtherThanLowestPoint.length; j++){
+            angleWithOtherPointsExceptLowestPoint[j] = 
+                    getAngleBetweenLowestPointAndTargetPoint(pointsOtherThanLowestPoint[j]);
+        }
+        initialMap.put(pointsOtherThanLowestPoint, angleWithOtherPointsExceptLowestPoint);
+        
+        //now sort the above map based on ascending order of angle.
+        
+         pointsOtherThanLowestPoint[i] = pointt;
+                angleWithOtherPointsExceptLowestPoint[i] = 
+                    getAngleBetweenLowestPointAndTargetPoint(pointt);
+                ++i;
+        
+        return list;
+    }
+
+    public ArrayList<Point> preparePolygonPoints() {
         ArrayList<Point> list = new ArrayList<Point>();
         list.add(new Point(100, 1000));
         list.add(new Point(-3, 842));
@@ -21,20 +71,22 @@ public class ViewPolygonWithTestPoint extends JFrame {
         list.add(new Point(600, 8));
         list.add(new Point(-100, 4));
         list.add(new Point(-500, 8));
-        Point pSmallest = list.get(0);
-        for(Point pp : list){
-            if(pSmallest.getY() >= pp.getY())
-                pSmallest = pp;
-        }
-        System.out.println("Smallest point is : "+ pSmallest.getX() + ","+ pSmallest.getY());
-        
         
         return list;
     }
 
-    private static Point getTestPoint() {
+    private Point getTestPoint() {
         Point testPoint = new Point(3, 3);
         return testPoint;
+    }
+    
+    public double getAngleBetweenLowestPointAndTargetPoint(Point p){
+        double angle = Math.atan((
+                (p.getY()-lowestPoint.getY())
+                        /
+                        (p.getX() - lowestPoint.getX())
+                ));
+        return angle;
     }
 
     public ViewPolygonWithTestPoint() {
@@ -49,8 +101,7 @@ public class ViewPolygonWithTestPoint extends JFrame {
         ArrayList<Point> points = new ArrayList<Point>();
         points = getPolygonPoints();
         Point testPoint = new Point(3, 3);
-        
-        
+
 //        testPoint = getTestPoint();
         //arrange points in order to plot polygon.
         // but now draw as usual.
@@ -60,7 +111,7 @@ public class ViewPolygonWithTestPoint extends JFrame {
         }
 //        System.out.println("--"+testPoint.getX());
 //        gd.drawLine(testPoint.getX(), testPoint.getY(), testPoint.getX()+2, testPoint.getY());
-         gd.drawLine(2, 200, 6, 119);
+        gd.drawLine(2, 200, 6, 119);
 
 //        for(int i = 0; i< polygonPoints.length; i++)
 //        gd.drawLine(polygonPoints[i].getX(), polygonPoints[i].getY(), WIDTH, WIDTH);
